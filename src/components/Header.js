db4f9bf0b6a55c174f0d0993716f07ed41d2964c
leela <nleela958@gmail.com>
2024-11-07 13:13:31 +0530

    changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = ({ username }) => {
   return (
     <header className="header">
       <div className="header-left">
-        <img src="/logo.png" alt="Logo" className="logo" />
+        <img src="https://i.pinimg.com/originals/26/d0/2f/26d02f43993c1983e294a2b6ded39d22.png" alt="Logo" className="logo" />
         <h1 className="welcome-message">{`Welcome, ${username}`}</h1>
       </div>
       <div className="header-center">
